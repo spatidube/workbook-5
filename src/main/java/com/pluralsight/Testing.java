c048ad87994fd444e67ccf4f0fb5094beda1d9f1
Spati Dube <sdube@appdev.yearup.org>
2025-05-13 11:53:30 -0500

    personal changes

--- src/main/java/com/pluralsight/Testing.java
+++ src/main/java/com/pluralsight/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
     public static void main(String[] args) {
         System.out.println("\\u001B[34m╔══════════════════════════════╗\\u001B[0m");
-        System.out.println("           ║   PART 1: Using Setters      ║");
+        System.out.println("           ║   WHICH IS WHICH-VEHICLE      ║");
         System.out.println("\\u001B[34m╚══════════════════════════════╝\\u001B[0m");
     }
 }
